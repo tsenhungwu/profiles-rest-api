99e0c95a162ebdf81dbd718b7303171552b0df55
tsenhungwu <bellyapply@gmail.com>
2020-12-16 21:37:44 -0500

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
+from profiles_api import serializers, models, permissions
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -86,3 +87,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an ojbect"""
         return Response({'http_method': "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
